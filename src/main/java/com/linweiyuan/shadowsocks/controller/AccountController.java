d200f4ce05d41cd56eb2019dd3cd3b717710f816
linweiyuan <root@linweiyuan.com>
2019-06-05 02:45:15 +0800

    页面展示

--- src/main/java/com/linweiyuan/shadowsocks/controller/AccountController.java
+++ src/main/java/com/linweiyuan/shadowsocks/controller/AccountController.java
@@ -2,12 +2,14 @@ package com.linweiyuan.shadowsocks.controller;
 
 import com.linweiyuan.commons.model.R;
 import com.linweiyuan.shadowsocks.service.AccountService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class AccountController {
     private final AccountService accountService;
 
@@ -15,6 +17,7 @@ public class AccountController {
         this.accountService = accountService;
     }
 
+    @ResponseBody
     @GetMapping("/accounts")
     public R accounts() {
         return accountService.findAll();
@@ -24,5 +27,11 @@ public class AccountController {
     @GetMapping("/accounts/{page}")
     public R page(@PathVariable int page) {
         return accountService.findByPage(page);
+    }
+
+    @GetMapping
+    public String index(Model model, @RequestParam(required = false, defaultValue = "1") int page) {
+        model.addAttribute("r", accountService.findByPage(page));
+        return "shadowsocks";
     }
 }
